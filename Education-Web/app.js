333e77a847f016d8ff332084a95e929bb091dd9c
Dua Shaikh <99713184+minahilirfan26@users.noreply.github.com>
2023-06-05 17:24:21 +0500

    Add files via upload
    https://fabulous-rugelach-9a647c.netlify.app/

---
+++ Education-Web/app.js
@@ -0,0 +1,8 @@
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function() {
+    navBar = document.querySelector(".navigation");
+    navBar.classList.toggle("active");
+}
+
+
+
