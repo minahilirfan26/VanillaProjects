b27ea4ede5ebba009b74af781db56825142db9a0
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2023-08-16 21:50:38 +0500

    https://64dc79d3755a4a3bac2107ae--remarkable-marzipan-21946f.netlify.app/

---
+++ Responsive Admin Dashboard/orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF182 Drone',
+        productNumber: '38788',
+        paymentStatus: 'Refunded',
+        shipping: 'Decline'
+    },
+    {
+        productName: 'Ruko Fil Pro Drone',
+        productNumber: '48758',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '87755',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4KDrone',
+        productNumber: '58328',
+        paymentStatus: 'PAid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF182 Drone',
+        productNumber: '38788',
+        paymentStatus: 'Refunded',
+        shipping: 'Decline'
+    }
+]
